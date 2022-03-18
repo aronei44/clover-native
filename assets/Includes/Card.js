73d29f9b3dd97cdd09167dbaee3576fe68ef16e3
aronei44 <meliodasmeliodas224@gmail.com>
2022-03-18 17:43:03 +0700

    feat (api) : integrated with clover product endpoint

--- assets/Includes/Card.js
+++ assets/Includes/Card.js
@@ -2,20 +2,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-const Card = () => {
+const Card = ({ id, name, price, image }) => {
     return (
         <View style={styles.card}>
             <View style={styles.cardHeader}>
                 <Image
                     source={{
-                        uri: 'https://drive.google.com/uc?id=1xjgbRKPYQrMM4ybE8_6hLQUuu8NQfsvT&export=media',
+                        uri: image,
                     }}
                     style={styles.cardLogo}
                 />
             </View>
             <View style={styles.cardBody}>
-                <Text style={styles.cardBodyTitle}>nama produk</Text>
-                <Text>harga</Text>
+                <Text style={styles.cardBodyTitle}>{name}</Text>
+                <Text style={styles.cardBodyPrice}>Rp {price}</Text>
             </View>
             <View style={styles.cardFooter}>
                 <Button color="#2f5000" title="Buy Now" />
@@ -55,9 +55,21 @@ const styles = StyleSheet.create({
     cardBodyTitle: {
         fontSize: 18,
         fontWeight: '600',
+        marginBottom: 5,
+    },
+    cardBodyPrice: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#2f5000',
+        marginVertical: 5,
+        marginBottom: 30,
     },
     cardFooter: {
         padding: 5,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
     },
     cardFooterButton: {
         marginHorizontal: 5,
--- assets/Pages/Home.js
+++ assets/Pages/Home.js
@@ -5,15 +5,29 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Card from '../Includes/Card';
+import axios from 'axios';
 
 
 const Home = () => {
+    const [data, setData] = React.useState([]);
+    axios.get('https://dev-api-clover.herokuapp.com/api/products')
+        .then(res => {
+            setData(res.data.data.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
     return (
         <View style={styles.container}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {data.map(item => (
+                <Card
+                    key={item.id}
+                    id={item.id}
+                    name={item.product_name}
+                    price={item.product_price}
+                    image={item.photos[0].product_image_path}
+                />
+            ))}
         </View>
     );
 };
